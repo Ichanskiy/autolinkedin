a2c89c57607df2d54fba9ee876943f667b1a4d98
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-10-29 14:03:32 +0200

    add ICompanyHeadcountRepository

---
+++ src/tech/mangosoft/autolinkedin/db/repository/ICompanyHeadcountRepository.java
@@ -0,0 +1,13 @@
+package tech.mangosoft.autolinkedin.db.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import tech.mangosoft.autolinkedin.db.entity.CompanyHeadcount;
+
+import java.util.List;
+
+public interface ICompanyHeadcountRepository extends CrudRepository<CompanyHeadcount, Long> {
+
+    List<CompanyHeadcount> findAll();
+    CompanyHeadcount getById(Long id);
+
+}
